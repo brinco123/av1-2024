5f8a1548e4852e1e6dee6ce3b8450251c7fa076e
brinco123 <108474868+brinco123@users.noreply.github.com>
2024-05-15 14:59:32 -0300

    s

---  main.js
+++  main.js
@@ -3,6 +3,44 @@ const botoes = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i<botoes.length; i++){
     botoes [i].onclick = function(){
-        
+        for(let j= ){
+            botoes
+            textos   
+        }        
+    botoes[i].
+    textos[i].classList.add("ativo");
     }
+}
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-28T23:59:59");
+const tempoObjetivo2 = new Date("2024-07-05T23:59:59");
+const tempoObjetivo3 = new Date("2024-08-01T23:59:59");
+const tempoObjetivo4 = new Date("2024-12-01T23:59:59");
+
+const tempos = [
+    tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+]
+
+for(let i=0; i<contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempoObjetivo1);
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo1){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + "dias"
+    + horas + "horas"
+    + minutos + "minutos"
+    + segundos + "segundos"
+
 }
